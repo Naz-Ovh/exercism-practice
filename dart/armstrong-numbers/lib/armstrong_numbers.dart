603bcefdc5a06d62e310214c30d79f6db51ec07e
Naz-Ovh <hi@naz.ovh>
2026-08-27 04:12:25 +0600

    dart: made armstrong number belongs to naz-ovh

--- dart/armstrong-numbers/lib/armstrong_numbers.dart
+++ dart/armstrong-numbers/lib/armstrong_numbers.dart
@@ -1,3 +1,28 @@
 class ArmstrongNumbers {
   // Put your code here
+  //
+  bool isArmstrongNumber(String input) {
+    if (input == "0") return true;
+
+    // one digit is always armstrong
+    if (input.length == 1) return true;
+
+    // two digit numbers are never armstrong number
+    if (input.length == 2) return false;
+
+    var power = input.length;
+
+    BigInt sum = BigInt.zero;
+
+    for (int i = 0; i < input.length; i++) {
+      var digit = int.tryParse(input[i]);
+      if (digit == null) {
+        return false;
+      }
+      var bigDigit = BigInt.from(digit);
+      sum += bigDigit.pow(power);
+    }
+
+    return sum.toString() == input;
+  }
 }
